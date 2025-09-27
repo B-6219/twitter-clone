5db9e2bbcc73e5802417563e515c22c7758acda1
B-6219 <brayokama6219@gmail.com>
2025-09-27 12:24:23 +0300

    server is ready

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Server is Running");
+import exress from 'express';
+
+const app = exress();
+const PORT = 5000;
+
+app.get('/',(req,res) =>{
+    res.send("Server is Ready")
+})
+
+app.listen(PORT || 8000 ,() => {
+    console.log("Server is Running Sucessfully");   
+})
