34fa9d8e5079c0cbd2acf1bdb799fea72e0cae0e
B-6219 <brayokama6219@gmail.com>
2025-12-13 23:32:05 +0300

    mongodb

--- backend/db/connectMongoDb.js
+++ backend/db/connectMongoDb.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const connectMongoDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log('Mongo DB connected sucessfully' );
+        const conn = await mongoose.connect(process.env.mongodb_uri )
+        console.log(`Mongo DB connected sucessfully ${conn.connection.host}`);
         
     } catch (error) {
         console.error(`Error connect ${error.message}`)
---
+++ backend/models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema( {
+    username : {
+        type:String,
+        required:true,
+        unique : true,
+    },
+    fullname: {
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        miniLength: 12,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique : true
+    },
+    follower: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'user',
+            default: []
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'user',
+            default: []
+        }
+    ],
+
+    profileImg:{
+        type:String,
+        default:" "
+    },
+    coverImg:{
+        type:String,
+        default:" "
+    },
+    bio:{
+        type:String,
+        default:" "
+    },
+    link:{
+        type:String,
+        default:" "
+    },
+
+} , {timestamps : true})
+
+const User = mongoose.model("User" , userSchema)
