3de028630ca91e607e3ffa2364ca569be4101e8a
B-6219 <brayokama6219@gmail.com>
2025-12-14 00:22:24 +0300

    creating signup logic also creation of token and cockies

--- backend/controllers/auth.controler.js
+++ backend/controllers/auth.controler.js
@@ -1,7 +1,57 @@
+import bcrypt from 'bcryptjs';
+import User  from '../models/user.model.js';
+import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
+
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "Signup Route",
-  });
+  try {
+    const {fullName , username , email , password} = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailRegex.test(email)){
+      return res.status(400).json({message: "Invalid email format"});
+    }
+
+    const existingUser = await User .findOne({username});
+    if(existingUser){
+      return res.status(400).json({message: "Username already taken"});
+    }
+
+    const existingEmail = await User .findOne({email});
+    if(existingEmail){
+      return res.status(400).json({message: "Email already taken"});
+    }
+
+    //hashed password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password , salt);
+
+    const newUser = new User ({
+      fullName,
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    if(newUser){
+      generateTokenAndSetCookie(newUser._id , res);
+      await newUser.save();
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({message: "Server Error"});
+    
+  }
 }
 
 export const login = async (req, res) => {
---
+++ backend/lib/utils/generateToken.js
@@ -0,0 +1,13 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+
+export const generateTokenAndSetCookie = (userId, res) => {
+    const token = JsonWebTokenError.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '15d',
+    });
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+    });
+}
