364a55853274ae0a79aab6e8acd86cca6c888c9c
B-6219 <brayokama6219@gmail.com>
2025-12-13 23:48:23 +0300

    api is working

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-import exress from "express";
+import express from "express";
 import authRoutes from "./routes/auth.router.js";
 import connectMongoDb from "./db/connectMongoDb.js";
 
-const app = exress();
+const app = express();
 const PORT = 5000;
 
-app.use("api/auth", authRoutes);
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
-app.get('/' ,(req,res) => {
-  res.send('hello')
-})
-
-app.listen(PORT || 8000, () => {
-  console.log("Server is Running Sucessfully");
-  connectMongoDb()
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
+app.listen(PORT, () => {
+  console.log("Server is running successfully");
+  connectMongoDb();
+});
