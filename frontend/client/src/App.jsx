cf5d79eab410c62b36dd1b2f2515919fee0e870b
B-6219 <brayokama6219@gmail.com>
2025-12-20 19:35:40 +0300

    experienced an error in finding the path of dummy js

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -5,17 +5,23 @@ import { Routes, Route } from 'react-router-dom'
 import SignUpPage from './auth/SignUpPage.jsx'
 import LoginPage from './auth/LoginPage'
 import Home from './auth/home'
+import Sidebar from './assets/components/common/sidebar.jsx'
+import RightPanel from './assets/components/common/RightPanel.jsx'
 
 
 const App = () => {
   return (
     <div className='flex max-w-6x1 mx-auto'>
 
+      <Sidebar />
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignUpPage />} />
       </Routes>
+
+      <RightPanel />
     </div>
   )
 }
