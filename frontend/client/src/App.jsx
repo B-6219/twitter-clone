7a5c4a7184379b08ec9083853d95bc00321931ae
B-6219 <brayokama6219@gmail.com>
2025-12-20 16:56:33 +0300

    i guess i can finish frontend today

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+
+import SignUpPage from './auth/SignUpPage.jsx'
+import LoginPage from './auth/LoginPage'
+import Home from './auth/home'
+
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-      <h1 className='text-black'>Ddrhgrg</h1>
-      <button className="btn btn-info">Info</button>
+    <div className='flex max-w-6x1 mx-auto'>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/client/src/assets/components/common/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div>
+            Psot
+        </div>
+    )
+}
+
+export default Post
