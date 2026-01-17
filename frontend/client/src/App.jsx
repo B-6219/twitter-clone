6e0a290e2546606d66fd70752ea94d7c171bd545
B-6219 <brayokamau6219@gmail.com>
2026-01-17 03:02:07 -0500

    ..

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -5,7 +5,8 @@ import { Routes, Route } from 'react-router-dom'
 import SignUpPage from './auth/SignUpPage.jsx'
 import LoginPage from './auth/LoginPage'
 import Home from './auth/home.jsx'
-import Sidebar from './assets/components/common/sidebar.jsx'
+import Sidebar from './assets/components/common/Sidebar.jsx'
+//import Sidebar from './assets/components/common/sidebar.jsx'
 import RightPanel from './assets/components/common/RightPanel.jsx'
 import NotificationPage from './assets/components/pages/notifications/NotificationPage.jsx'
 import ProfilePage from './assets/components/pages/profile/ProfilePage.jsx'
@@ -13,7 +14,7 @@ import ProfilePage from './assets/components/pages/profile/ProfilePage.jsx'
 
 const App = () => {
   return (
-    
+
     <div className='flex max-w-6x1 mx-auto'>
 
       <Sidebar />
